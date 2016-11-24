d3e2d51738914abfe421cbf1b298e0b9b0f997c5
akyekth <akye@kth.se>
2016-11-24 14:16:29 +0100

    Update CompareSignature.scala

--- SimilarItems_hw1_submit/src/main/scala/com/id2222/CompareSignature.scala
+++ SimilarItems_hw1_submit/src/main/scala/com/id2222/CompareSignature.scala
@@ -3,11 +3,25 @@ package com.id2222
 class CompareSignature{
 	
 	def comparetwovectors(doc1:List[Int],doc2:List[Int]):Double= {
-		val intersection=((doc1.intersect(doc2)).length).toDouble
-		//println("Minhash _ Similarity intersection::::::::::"+intersection)
-		val union =((doc1.union(doc2)).distinct.length).toDouble
-		val Minhash_sim:Double=intersection/union
-		return Minhash_sim
+		
+	  var counter:Double=0
+	  for(i<- 0 to doc1.length-1)
+	  {
+	  
+	    if(doc1(i) == doc2(i))
+	    {
+	      counter +=1
+	    }
+	  
+	  }
+	  return counter/(doc1.length).toDouble
+	  
+	  
+//	  val intersection=((doc1.intersect(doc2)).length).toDouble
+//		//println("Minhash _ Similarity intersection::::::::::"+intersection)
+//		val union =((doc1.union(doc2)).distinct.length).toDouble
+//		val Minhash_sim:Double=intersection/union
+//		return Minhash_sim
 		
 	}
   
